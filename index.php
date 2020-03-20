86b45265f1236cb52b298b72ffd6a48cd0bf6038
David Harrison <david.harrison1992@gmail.com>
2020-03-20 22:44:12 +0000

    Created files

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title>Socially Distant Pub Quiz</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+<form class="form-horizontal">
+
+
+<div class="row">
+  <div class="col-xs-12 col-md-8"><h1>Socially Distant Pub Quiz</h1>
+<h3><em>Now with 100% less human contact</em></h3></div>
+
+<div class="col-xs-6 col-md-4">
+  	<select class="form-control">
+  <option>Pick team...</option>
+  <option>Quiz on my face...</option>
+  <option>Quizzeeeee Rascalz</option>
+  <option>Chris Whitty's Titties</option>
+  <option>Risky Sunak</option>
+</select>
+
+  <div class="form-group">
+    <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    </div>
+  </div>
+
+
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+</form> <!-- this whole thing was a form?!  -->
+</div> <!-- /container -->
+</body>
+</html>
+
