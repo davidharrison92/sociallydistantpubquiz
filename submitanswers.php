c196f8be7a769b08b517574e526748a30bdf1e53
David Harrison <david.harrison1992@gmail.com>
2020-03-22 22:17:55 +0000

    build array

--- submitanswers.php
+++ submitanswers.php
@@ -2,3 +2,42 @@
 
 include("db/db_config.php");
 
+// clean the results
+
+
+if ( empty($_POST) ) {
+
+  $error = true;
+  $error_reason[] = "Please enter the details on the form below";
+
+  echo "nothing set";
+} else {
+
+// CLEAN IT, BUILD AN ARRAY
+
+	$answers["a1"] = mysqli_real_escape_string($conn,$_POST["ans1"]);
+	$answers["a2"] = mysqli_real_escape_string($conn,$_POST["ans2"]);
+	$answers["a3"] = mysqli_real_escape_string($conn,$_POST["ans3"]);
+	$answers["a4"] = mysqli_real_escape_string($conn,$_POST["ans4"]);
+	$answers["a5"] = mysqli_real_escape_string($conn,$_POST["ans5"]);
+	$answers["a6"] = mysqli_real_escape_string($conn,$_POST["ans6"]);
+	$answers["a7"] = mysqli_real_escape_string($conn,$_POST["ans7"]);
+	$answers["a8"] = mysqli_real_escape_string($conn,$_POST["ans8"]);
+	$answers["a9"] = mysqli_real_escape_string($conn,$_POST["ans9"]);
+	$answers["a10"] = mysqli_real_escape_string($conn,$_POST["ans10"]);
+
+
+	$answers["teamID"] = mysqli_real_escape_string($conn, $_POST["teamID"]);
+	$answers["teamsecret"] = mysqli_real_escape_string($conn, $_POST["secret"]);
+
+
+	} // end of if POST empty.
+
+
+var_dump($answers);
+
+
+
+// loop through the array, and pass them to the database.
+
+
