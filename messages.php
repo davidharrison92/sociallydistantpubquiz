903f44b12c34afc4274e9a7d8a125b4ed02cccce
David Harrison <david.harrison1992@gmail.com>
2020-03-31 21:02:57 +0100

    fixed bug with reopening sessions, or empty messages set

--- messages.php
+++ messages.php
@@ -1,14 +1,19 @@
 
 <?php 
-session_start();
-foreach($_SESSION["messages"] as $key => $value){
-    foreach ($value as $message){
-    ?>
-        <div class="alert alert-<?php echo $key; ?>" role="alert">
-            <p><?php echo $message; ?></p>
-        </div>
-    <?php
-    }
+if(!isset($_SESSION)){
+	session_start();	
+}
+
+if (isset($_SESSION["messages"])){
+	foreach($_SESSION["messages"] as $key => $value){
+	    foreach ($value as $message){
+	    ?>
+	        <div class="alert alert-<?php echo $key; ?>" role="alert">
+	            <p><?php echo $message; ?></p>
+	        </div>
+	    <?php
+	    }
+	}
 }
 unset($_SESSION["messages"]);
 ?>
