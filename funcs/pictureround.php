68518a0de3bace5f8bbdb7dcc3dc1ed82627fc91
David Harrison <david.harrison1992@gmail.com>
2020-04-09 22:53:43 +0100

    Image as icon on marking page

--- funcs/pictureround.php
+++ funcs/pictureround.php
@@ -19,4 +19,20 @@ function pictureround($imagesrc){
 		return 'Error - supplied image is not an allowable file format';
 	}
 
-}
+}	
+
+
+
+function pictureicon($imagesrc){
+	$allow_types = array('PNG', 'JPG', 'JPEG');
+
+
+
+	if (in_array(strtoupper(get_extension($imagesrc)), $allow_types))	{
+		$outputstring = '<img src="picture_round/' . $imagesrc . '" alt="Picture Question" class="img-thumbnail" height="50px" width="50px">';
+		return $outputstring;
+	} else {
+		return 'Error - supplied image is not an allowable file format';
+	}
+
+}	
