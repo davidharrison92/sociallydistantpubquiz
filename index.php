9d05ecc3c919f391c0eb521366d69093731c2640
David Harrison <david.harrison1992@gmail.com>
2020-04-10 16:11:55 +0100

    show count of teams

--- index.php
+++ index.php
@@ -77,20 +77,21 @@ if (array_key_exists("teamID", $_SESSION)){
                 if ($allow_signup == 1) {
                 ?>
                 <div class="row">
-                    <p class="lead">Teams registered so far: </p>
+                    <p class="lead"><strong><?php echo count($teams_list); ?> teams</strong> registered already! </p>
                     <div class="col-md-4">
                         <ul>
                             <?php
                             $teamprinter = 0;
                             foreach($teams_list as $team){
                                 $teamprinter = $teamprinter + 1;
-                                if (count($teams_list) > 14 and $teamprinter % ceil(count($teams_list)/3) == 0 and $teamprinter > 0){
+                                if (count($teams_list) > 14 and $teamprinter % ceil((count($teams_list)+1)/3) == 0 and $teamprinter > 0){
                                     // every 10th row, start a new column
                                     echo '</div><div class="col-md-4">';
                                 }
                                 echo "<li>".$team["team_name"]."</li>";
                             }
                                 ?>
+                          <li><a href="newteam.php">Click here to become team #<?php echo count($teams_list)+1; ?>...</a> </li>
                         </ul>
                     </div>
                 </div> 
