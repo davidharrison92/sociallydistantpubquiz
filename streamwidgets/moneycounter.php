dff1810f9aa80b2b44f16028700802b8bd6cfbab
David Harrison <david.harrison1992@gmail.com>
2020-04-14 22:03:30 +0100

    Example, no backend

---
+++ streamwidgets/moneycounter.php
@@ -0,0 +1,40 @@
+<?php
+
+include("../db/db_wordpress.php");
+
+?>
+
+
+<html>
+<head>
+        <title>Socially Distant Pub Quiz | Live Total</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/site.webmanifest">
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+
+</head>
+<body>
+	<div class="container">
+		<h1>Current Total: £560!   (£440 to go until baldness ensues!)</h1>
+
+		<h3>Last Donation: Alex L, £10! Thank You!</h3>
+
+		<h4>Biggest Donors so far:</h4>
+		<ul>
+			<li>Dave H - £100</li>
+			<li>Jonny A - £34</li>
+			<li>Stevie J - £23</li>
+		</ul>
+	</div>
+</body>
+
+
+</html>
