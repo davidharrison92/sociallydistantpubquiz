b52157ea16f6dfe2e418a3d929fd21dbcf8b1282
David Harrison <david.harrison1992@gmail.com>
2020-04-16 13:18:30 +0100

    stream tweaks

--- streamwidgets/moneycounter.php
+++ streamwidgets/moneycounter.php
@@ -12,6 +12,8 @@
             src="https://code.jquery.com/jquery-3.4.1.min.js"
             integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
             crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="stream.css">
+
 
 </head>
 <body>
--- streamwidgets/runningtotals.php
+++ streamwidgets/runningtotals.php
@@ -49,6 +49,7 @@ if ($charity_total > 350 and $charity_total <= 1000) {
 <html>
 	<head>
 		        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		        <link rel="stylesheet" href="stream.css">
 	</head>
 
 <body>
