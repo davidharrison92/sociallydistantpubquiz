961d0fc73e42f4a68373b3370954a73b746ea979
David Harrison <david.harrison1992@gmail.com>
2020-04-20 21:32:34 +0100

    Half written code. Needs work

--- db/build_minileague.php
+++ db/build_minileague.php
@@ -1,17 +1,17 @@
 <?php
 include("db/db_config.php");
-include("../funcs/leaderboard_functions.php");
 
 if (!isset($current_round)){
 	// this won't be needed if embeded on index, but will be needed if running in standalone window.
 	include("db/get_game_state.php");
 }
 
+include("../funcs/leaderboard_functions.php")
 
 
 // this just spits out a table.
 
-$lbq = "select team_id, team_name, person1, person2, person3, person4, total_score, total_marked, R1Correct, R1Marked, R2Correct, R2Marked, R3Correct, R3Marked, R4Correct, R4Marked, R5Correct, R5Marked, R6Correct, R6Marked, R7Correct, R7Marked, R8Correct, R8Marked, R9Correct, R9Marked from complex_leaderboard" ;
+$lbq = "select team_id, team_name, person1, person2, person3, person4, total_score, total_marked, R1Correct, R1Marked, R2Correct, R2Marked, R3Correct, R3Marked, R4Correct, R4Marked, R5Correct, R5Marked, R6Correct, R6Marked, R7Correct, R7Marked, R8Correct, R8Marked, R9Correct, R9Marked from complex_leaderboard cl JOIN mini_league on cl.team_id = ml.league_member" ;
 
 
 
@@ -27,20 +27,9 @@ $current_rank = 0;
 $repeats = 1;
 ?>
 
-
-
 <table class="table table-condensed table-hover">
     <tr>
 		<td><strong>Rank</strong></td>
-		<?php 
-		if (array_key_exists("teamID", $_SESSION)){
-			?>
-			  <form action="leaderboard.php" method="POST">
-			  <td>
-			  	<button type="submit" class="btn btn-default btn-xs">+ My<br>League</button>
-			  </td>
-			 <?php
-		} ?> 
         <td><strong>Team Name</strong></td>
 		<?php 
 			for ($i = 1; $i <= $current_round; $i++){
@@ -83,7 +72,6 @@ foreach($leaderboard as $lb){
 
 <tr>
 	<td><p><?php echo $current_rank; ?></p></td>
-	<?php echo add_team($lb["team_id"]); ?>
 	<td><p><strong><?php echo $lb["team_name"];?></strong>
 		<span class="small"><?php echo $teammembers; ?></span> </p>
 
@@ -101,11 +89,5 @@ foreach($leaderboard as $lb){
 
 	<?php
 }
-
-	if (array_key_exists("teamID",$_SESSION)){
-		echo '</form>';
-	}
-
 ?>
-
-</table>
+</table
--- leaderboard.php
+++ leaderboard.php
@@ -1,6 +1,46 @@
 <?php
+session_start();
 include("db/db_config.php");
 include("db/get_game_state.php");
+
+//Build Mini League on form submit
+
+    if (array_key_exists("addteam", $_POST) and array_key_exists("teamID",$_SESSION)){
+        
+        foreach($_POST["addteam"] as $entry) {
+            $ins_array[] = "('".$_SESSION["teamID"]."' , '".$entry."')";
+        }
+
+       // var_dump($ins_array);
+        $ins_qry = 'INSERT IGNORE mini_leagues(league_owner,league_member) VALUES' . implode(" , ", $ins_array) ;
+
+        if(mysqli_query($conn, $ins_qry)){
+            // inserted succesfully
+        } else {
+            // failed to insert - raise an error
+        }
+
+    }
+
+// Check if there's a Mini League for this team ID
+
+if (array_key_exists("teamID", $_SESSION)){
+    $qry_check_ML = "SELECT COUNT(*) as 'Count' FROM mini_leagues where league_owner = '".$_SESSION["teamID"]."'; ";
+
+    $check_ML = $conn->query($qry_check_ML);
+    $check_ML = $check_ML->fetch_assoc();
+
+    if ($check_ML["Count"] * 1  >= 1) {
+        $mini_league_exists = true;
+    } else {
+        $mini_league_exists = false;
+    }
+
+} else {
+    //no team logged in
+    $mini_league_exists = false;
+}
+
 ?>
 <head>
     <title>Socially Distant Pub Quiz</title>
@@ -10,11 +50,31 @@ include("db/get_game_state.php");
     <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
     <link rel="manifest" href="favicon/site.webmanifest">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <div class="container">
-        <h1>Leaderboard:</h1>
-        <h4>Socially Distanced Pub Quiz</h4>
+
+
+<div>
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#main" aria-controls="main" role="tab" data-toggle="tab">Full Leaderboard</a></li>
+    <li role="presentation"><a href="#mini" aria-controls="mini" role="tab" data-toggle="tab">My League</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="main">
+
+        <h1>Main Leaderboard:</h1>
+        <h4>Socially Distanced Pub Quiz - Overall</h4>
 
         <?php
         if ($current_round > 1){
@@ -28,6 +88,36 @@ include("db/get_game_state.php");
         }
         ?>
             
+    </div>
+
+
+    <div role="tabpanel" class="tab-pane" id="mini">prof</div>
+
+
+
+        <h1>Mini Leaderboard:</h1>
+        <h4>Compete against the teams that matter...</h4>
+
+        <?php
+        if ($current_round > 1){
+         include("db/build_minileague.php"); 
+        } else {
+        ?>
+        <div class="alert alert-warning"><p><strong>The results aren't in yet</strong></p>
+            <p>The leaderboard will become available after the first round has been marked</p>
+        </div>
+        <?php
+        }
+        ?>
+
+
+
+  </div>
+
+</div>
+
+
+
 
     </div> <!-- end container -->
 </body>
