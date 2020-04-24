cfa1bb0c7078dc7a42260f6eeca676cddb703dc1
David Harrison <david.harrison1992@gmail.com>
2020-04-24 19:08:06 +0100

    force nav to new row

--- header.php
+++ header.php
@@ -17,24 +17,26 @@ function site_tabs($current_round, $quiz_complete){
 ?>
 
 <div class="container">
-    <div class="col-xs-12 col-md-5">
-        <a href="index.php"><img src="thepanickedshopper.jpg" class="img-responsive img-circle" max alt="Responsive image" style="max-height: 200px; max-width: 200px;"></a>
-        <h1>Socially Distant Pub Quiz</h1>
-        <h4>Now with 100% less human contact</h4>
-    </div>
-    <div class="col-xs-12 col-md-5 pull-right">
-        <?php if ($show_video == 1) {
+     <div class="row">
+        <div class="col-xs-12 col-md-5">
+            <a href="index.php"><img src="thepanickedshopper.jpg" class="img-responsive img-circle" max alt="Responsive image" style="max-height: 200px; max-width: 200px;"></a>
+            <h1>Socially Distant Pub Quiz</h1>
+            <h4>Now with 100% less human contact</h4>
+        </div>
+        <div class="col-xs-12 col-md-6 pull-right">
+            <?php if ($show_video == 1) {
+                ?>
+            <iframe width="100%" height="300" id="ytembed" class="miniplayer" <?php echo 'src="https://www.youtube-nocookie.com/embed/'.$ytID .'?controls=0&autoplay=1" '; ?>frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <?php
+            } // end show vid
             ?>
-        <iframe id="ytembed" class="miniplayer" <?php echo 'src="https://www.youtube-nocookie.com/embed/'.$ytID .'?controls=0&autoplay=1" '; ?>frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <?php
-        } // end show vid
-        ?>
+        </div>
     </div>
     <div class="row">
         <div class="navbar-default">
             <ul class="nav navbar-nav">
-                <li><a href="https://sociallydistant.pub/store/" target="_blank"><strong>Raffle</strong></a></li>
-                <li><a href="about.php" <?php echo site_tabs($current_round, $quiz_complete); ?> >How To Play</a></li>
+<!--                 <li><a href="https://sociallydistant.pub/store/" target="_blank"><strong>Raffle</strong></a></li>
+ -->            <li><a href="about.php" <?php echo site_tabs($current_round, $quiz_complete); ?> >How To Play</a></li>
                 <li><a href="your_answers.php" <?php echo site_tabs($current_round, $quiz_complete); ?> >Your Score</a></li>
                 <li><a href="leaderboard.php"  <?php echo site_tabs($current_round, $quiz_complete); ?> >Full Leaderboard</a></li>
                 <li class="navbar-right"><a href="https://twitter.com/davidharrison92" target="_blank">@Dave</a></li>
