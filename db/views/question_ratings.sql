4a8a1cba44a3fc9251657ce42dee0290b5823133
David Harrison <david.harrison1992@gmail.com>
2020-05-22 15:16:21 +0100

    DB View to count them quickly

---
+++ db/views/question_ratings.sql
@@ -0,0 +1,16 @@
+CREATE VIEW question_popularity AS
+
+	SELECT
+	    q.round_number,
+		q.question_number,
+		COUNT(
+			r.team_id
+		)
+	FROM
+		quiz_questions q
+		LEFT JOIN question_ratings r ON r.question_number = q.question_number
+		AND r.round_number = q.round_number
+	GROUP BY
+		q.question_number,
+		q.round_number
+		
