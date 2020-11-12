f41c1506d3ec1f66d25d54cfe53d89f6f47efca1
David Harrison <david.harrison1992@gmail.com>
2020-11-12 21:09:27 +0000

    Front end

---
+++ manage_team.php
@@ -0,0 +1,204 @@
+<?php
+var_dump($_POST);
+include ("db/db_config.php");
+include ("db/get_game_state.php");
+include("db/get_teams.php");
+
+
+// This page has a login form - check if it has been submitted, and if needed, log the team in.
+if (isset($_POST["teamsecret"]) and isset($_POST["teamID"])){ 
+    $teamID = mysqli_real_escape_string($conn, $_POST["teamID"]); // watch out! - $teamID is the form value. $team_id is the Session value.
+    $teamsecret = mysqli_real_escape_string($conn,$_POST["teamsecret"]);
+    include("db/check_login.php");
+}
+
+
+// Changes to data
+    
+    IF (isset($_GET["change"])){
+
+        IF ($_GET["change"] == "secret"){
+            // Sanitise inputs
+
+            // check match of new and old secret
+
+            // set new secret
+
+
+        }
+
+
+
+
+        IF ($_GET["change"] == "team"){
+            // Sanitise Inputs
+            
+            //Build Query
+
+            // Run Query
+        }
+
+
+    }
+
+
+
+// Get team data
+
+$team_id = $_SESSION["teamID"];
+
+$team_qry = "SELECT team_name, person1, person2, person3, person4, team_email, email_opt_in from teams where team_id = '$team_id' limit 1";
+$team_qry = mysqli_query($conn, $team_qry);
+
+
+
+$team_data = mysqli_fetch_row($team_qry);
+
+$team_name = $team_data[0]; //team name
+
+//team members
+    $person1 = $team_data[1];
+    $person2 = $team_data[2];
+    $person3 = $team_data[3];
+    $person4 = $team_data[4];
+
+$team_email = $team_data[5];
+$email_opt_in = $team_data[6];
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edit Team | Socially Distant Pub Quiz</title>
+        <?php include("htmlheader.php"); ?>
+
+    </head>
+
+    <body>
+        <div class="container">
+                <div class="row">
+                    <?php include("header.php");?>
+                </div> <!-- / header row -->
+                <hr>
+                <div class="row"> <!-- main content -->
+                <?php if (array_key_exists("teamID", $_SESSION)){ ?>
+                    <div class="page-header">
+                        <h1>Edit Team: <small><?php echo $team_name; ?></small></h1>
+                    </div>
+
+                <div class="col-md-6" id="teaminfo">
+                    <h3>Team Details</h3> <button href="#" class="btn btn-info swapscript"><span class="glyphicon glyphicon-pencil"></span> Edit</button>
+
+                    <dl class="dl-horizontal">
+                        <dt>Team Name</dt>
+                            <dd><?php echo $team_name;?></dd>
+                        <dt>Team Members</dt>
+                            <dd><?php echo $person1;?></dd>
+                            <dd><?php echo $person2;?></dd>
+                            <dd><?php echo $person3;?></dd>
+                            <dd><?php echo $person4;?></dd>
+                        <dt>Team Email</dt>
+                            <dd><?php echo $team_email;?></dd>
+                    </dl>
+                    
+
+                    
+                </div>
+
+                <div class="col-md-12 hidden" id="teamedit">
+
+                    <h3>Change Team Detail</h3> 
+                    
+                    <form class="form-horizontal" action="manage_team.php?change=team" method="POST">
+                        <div class="form-group">
+                            <label for="teamName" class="col-sm-2 control-label">Team Name</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" id="teamName" placeholder="<?php echo $team_name; ?>" value="<?php echo $team_name;?> ">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="teamMembers" class="col-sm-2 control-label">Team Members</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="teamMember1" placeholder="<?php echo $person1;?>" value="<?php echo $person1; ?>">
+                                <input type="text" class="form-control" id="teamMember2" placeholder="<?php echo $person2;?>" value="<?php echo $person2; ?>">
+                                <input type="text" class="form-control" id="teamMember3" placeholder="<?php echo $person3;?>" value="<?php echo $person3; ?>">
+                                <input type="text" class="form-control" id="teamMember4" placeholder="<?php echo $person4;?>" value="<?php echo $person4; ?>">
+
+                            </div>
+                            
+                        </div>
+
+                        <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="teamEmail" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                            <input type="email" class="form-control" id="teamEmail" placeholder="<?php echo $team_email; ?>" value="<?php echo $team_email;?> ">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                            <div class="checkbox">
+                                <label>
+                                <input type="checkbox" value="<?php echo $email_opt_in; ?>"> I want to hear about future quizzes and other shenanigans.
+                                </label>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Save changes</button>
+                            </div>
+                        </div>
+                        </form>                    
+                    
+                        <hr>
+                        <h3>Change Team Secret</h3> 
+
+                        <form class="form-inline" method="POST" action="manage_team.php?change=secret">
+                            <div class="form-group">
+                                <label for="current_secret">Current Secret:</label>
+                                <input type="text" class="form-control" id="current_secret" placeholder="Hello There...">
+                            </div>
+                            <div class="form-group">
+                                <label for="new_secret">New Secret</label>
+                                <input type="text" class="form-control" id="new_secret" placeholder="General Kenobi...">
+                            </div>
+                            <button type="submit" class="btn btn-default">Change Secret</button>
+                            </form>
+                    
+                    <button href="#" class="btn btn-link swapscript"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+                </div>
+
+                <script>
+                    $(".swapscript").click(function(){
+                        $('#teaminfo').toggleClass("hidden");
+                        $('#teamedit').toggleClass("hidden");
+                    });
+                    
+                </script>
+                
+                <?php } else { ?>
+                        <!-- no team logged in -->
+
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">You must be logged in before you can make changes to your team.</h3>
+                            </div>
+                            <div class="panel-body">
+                            If you want to register a new team, please <a href="newteam.php">click here</a>.
+                            </div>
+                        </div>
+                        <?php
+                            include("loginform.php");
+                        }
+                    ?>
+
+                </div> <!-- /main content -->
+
+    </div> <!-- End Container -->
+    </body>
+</html>
